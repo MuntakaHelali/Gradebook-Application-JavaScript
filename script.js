8fd5afb73f9b87bde48d263f53635a27dd70bf16
MuntakaH <57158277+MuntakaHelali@users.noreply.github.com>
2024-09-09 23:50:31 -0400

    Updated Script.js
    Solved all challenges for JavaScript Gradebook Application using Conditional Statements, Loops, and Mathematic Algorithms.

---
+++ script.js
@@ -0,0 +1,41 @@
+function getAverage(scores) {
+    let sum =0;
+    for(const score of scores){
+        sum += score
+    }
+    return sum/scores.length;
+}
+
+function getGrade(score){
+    if(score === 100){
+        return "A++";
+    }
+    else if(score >=90){
+        return "A";
+    }
+    else if(score>=80){
+        return "B";
+    }
+    else if(score>=70){
+        return "C";
+    }
+    else if(score>=60){
+        return "D";
+    }
+    else{
+        return "F";
+    }
+}
+
+function hasPassingGrade(score){
+    return getGrade(score) != "F";
+}
+
+function studentMsg(totalScores, studentScore) {
+    let classAverage = getAverage(totalScores);
+      let sScore = getGrade(studentScore);
+      if(sScore !="F"){
+          return "Class average: "+classAverage+". Your grade: "+sScore+". You passed the course.";
+      }
+      return "Class average: "+classAverage+". Your grade: "+sScore+". You failed the course.";
+  }
